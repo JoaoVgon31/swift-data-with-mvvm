aee6a84361e92f8c69e382cc7b786e40c1210b90
João Vitor Gonçalves Oliveira <joaovitorgoliveira31@gmail.com>
2024-04-16 10:03:54 -0300

    Add project to git repository

---
+++ SwiftDataWithMVVM/DataSources/DataSource.swift
@@ -0,0 +1,29 @@
+import Foundation
+import SwiftData
+
+protocol DataSource {
+    var modelContainer: ModelContainer {get}
+    var modelContext: ModelContext {get}
+    
+    func append<T>(element: T) where T: PersistentModel
+    func fetch<T>(predicate: Predicate<T>?, sortBy sortDescriptors: [SortDescriptor<T>]) -> [T] where T: PersistentModel
+    func remove<T>(_ element: T) where T: PersistentModel
+}
+
+extension DataSource {
+    func append<T>(element: T) where T: PersistentModel {
+        modelContext.insert(element)
+    }
+    
+    func fetch<T>(predicate: Predicate<T>? = nil, sortBy sortDescriptors: [SortDescriptor<T>] = []) -> [T] where T: PersistentModel {
+        do {
+            return try modelContext.fetch(FetchDescriptor<T>(predicate: predicate ?? #Predicate {element in true}, sortBy: sortDescriptors))
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+
+    func remove<T>(_ element: T) where T: PersistentModel {
+        modelContext.delete(element)
+    }
+}
---
+++ SwiftDataWithMVVM/ViewModels/CommentsViewModel.swift
@@ -0,0 +1,31 @@
+import Foundation
+import SwiftData
+
+@Observable
+class CommentsViewModel {
+    @ObservationIgnored
+    private let dataSource: CommentDataSource
+
+    var comments: [Comment] = []
+    var showAddComment: Bool = false
+    
+    var didSavePublisher: NotificationCenter.Publisher {
+        NotificationCenter.default.publisher(for: Notification.Name.NSManagedObjectContextDidSave)
+    }
+
+    init(dataSource: CommentDataSource = CommentDataSource.shared) {
+        self.dataSource = dataSource
+    }
+    
+    func fetch(sortBy sortDescriptors: [SortDescriptor<Comment>] = []) {
+        comments = dataSource.fetch(sortBy: sortDescriptors)
+    }
+    
+    func fetch(predicate: Predicate<Comment>, sortBy sortDescriptors: [SortDescriptor<Comment>] = []) {
+        comments = dataSource.fetch(predicate: predicate, sortBy: sortDescriptors)
+    }
+
+    func remove(_ comment: Comment) {
+        dataSource.remove(comment)
+    }
+}
